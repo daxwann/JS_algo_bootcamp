12d2d386084bb73b4f908061bfeaa0a46ce8ab77
daxwann <daxwann@gmail.com>
2019-12-09 12:47:32 -0800

    complete anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let charMap = {};
+  const lowerCaseStrA = stringA.toLowerCase();
+  const lowerCaseStrB = stringB.toLowerCase();
+  debugger
+
+  for (let char of lowerCaseStrA) {
+    if (char.match(/[a-z]/)) {
+      charMap[char] = charMap[char] + 1 || 1
+    }
+  }
+
+  for (let char of lowerCaseStrB) {
+    if (char.match(/[a-z]/)) {
+      if (charMap[char]) {
+        charMap[char] -= 1
+      } else {
+        charMap[char] = -1
+      }
+    }
+  }
+
+  return Object.values(charMap).every(count => count === 0);
+}
+
+anagrams("hello", "olleh");
 
 module.exports = anagrams;
