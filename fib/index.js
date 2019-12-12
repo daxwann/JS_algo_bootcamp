6005d2a42715f50ca8a2c64a03ff3ee01c1956e5
daxwann <daxwann@gmail.com>
2019-12-11 16:00:55 -0800

    add fibonacci solutions

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,63 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// iterative -- runtime: O(n)
+// function fib(n) {
+//   let prevNum = 0;
+//   let currNum = 1;
+
+//   if (n === 0) {
+//     return prevNum;
+//   } else if (n === 1) {
+//     return currNum;
+//   }
+
+//   for (let i = 2; i <= n; i++) {
+//     let tmp = currNum;
+//     currNum = prevNum + currNum;
+//     prevNum = tmp;
+//   }
+
+//   return currNum;
+// }
+
+// recursive -- runtime O(2^n)
+// function fib(n) {
+//   if (n === 0) {
+//     return 0;
+//   } else if (n === 1) {
+//     return 1;
+//   }
+
+//   return fib(n - 2) + fib(n - 1);
+// }
+
+// recursive solution with memoization
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 2) + fib(n - 1);
+}
+
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    //return result if found in cache
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    // cache and return result
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result
+  }
+}
+
+fib = memoize(fib)
 
 module.exports = fib;
