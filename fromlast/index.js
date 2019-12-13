46ba18083ff6b3a14de974f4c9b5941d643102d0
daxwann <daxwann@gmail.com>
2019-12-12 19:42:23 -0800

    complete n from last

--- fromlast/index.js
+++ fromlast/index.js
@@ -11,6 +11,20 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  let slowPt = list.head;
+  let fastPt = list.head;
+  
+  for (let i = 0; i < n; i++) {
+    fastPt = fastPt.next;
+  }
+
+  while (fastPt && fastPt.next) {
+    fastPt = fastPt.next;
+    slowPt = slowPt.next;
+  }
+
+  return slowPt;
+}
 
 module.exports = fromLast;
